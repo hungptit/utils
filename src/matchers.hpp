02426e5668612bfd674590933189da126d4f891a
Hung Dang <hdang@athenahealth.com>
2018-03-22 23:37:14 -0400

    Add sse2 implementation of strstr

--- src/matchers.hpp
+++ src/matchers.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "string.hpp"
 #include <cstring>
+#include <string.hpp>
 #include <string>
 
 namespace utils {
@@ -10,67 +12,127 @@ namespace utils {
         bool operator()(const char *, const char *) { return true; }
     };
 
-	// Check that a searched string has a given pattern.
-    struct StartsWith {
-      public:
-        template <typename String>
-        explicit StartsWith(String &&patt) : pattern(std::forward<String>(patt)) {
-            begin = pattern.c_str();
-            end = begin + pattern.size();
-        }
-        bool operator()(const char *b, size_t len) {
-            if (len < pattern.size()) {
-                return 0;
+    namespace baseline {
+        // Check that a searched string has a given pattern.
+        class StartsWith {
+          public:
+            template <typename String>
+            explicit StartsWith(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) {
+                if (line.size() < pattern.size()) { return false; }
+                return strncmp(line.data(), pattern.data(), pattern.size()) == 0;
             }
-            return strncmp(b, begin, pattern.size()) == 0;
-        }
 
-      private:
-        const std::string pattern;
-        const char *begin;
-        const char *end;
-    };
+          private:
+            const std::string pattern;
+        };
 
-	// Search for a sub string.
-    struct Contains {
-      public:
-        template <typename String>
-        explicit Contains(String &&patt) : pattern(std::forward<String>(patt)) {
-            begin = pattern.c_str();
-            end = begin + pattern.size();
-        }
-        bool operator()(const char *beg, size_t len) {
-            if (len < pattern.size()) {
-                return 0;
+        // Search for a sub string.
+        class Contains {
+          public:
+            template <typename String>
+            explicit Contains(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) {
+                if (line.size() < pattern.size()) { return false; }
+                return line.find(pattern) != std::string::npos;
             }
-            return strstr(beg, begin) != NULL;
-        }
 
-      private:
-        const std::string pattern;
-        const char *begin;
-        const char *end;
-    };
+          private:
+            const std::string pattern;
+        };
 
-	// Check that two strings are equal.
-    struct Equals {
-      public:
-        template <typename String>
-        explicit Equals(String &&patt) : pattern(std::forward<String>(patt)) {
-            begin = pattern.c_str();
-            end = begin + pattern.size();
-        }
-        bool operator()(const char *beg, size_t len) {
-            if (len != pattern.size()) {
-                return 0;
+        // Check that two strings are equal.
+        class Equals {
+          public:
+            template <typename String>
+            explicit Equals(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) { return line == pattern; }
+
+          private:
+            const std::string pattern;
+        };
+    } // namespace baseline
+
+    namespace sse2 {
+        // Check that a searched string starts with a given pattern.
+        class StartsWith {
+          public:
+            template <typename String>
+            explicit StartsWith(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) {
+                if (line.size() < pattern.size()) { return false; }
+                return strncmp(line.data(), pattern.data(), pattern.size()) == 0;
             }
-            return strncmp(beg, begin, len) == 0;
-        }
 
-      private:
-        const std::string pattern;
-        const char *begin;
-        const char *end;
-    };
+          private:
+            const std::string pattern;
+        };
+
+        // Search for a sub string.
+        class Contains {
+          public:
+            template <typename String>
+            explicit Contains(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) {
+                return sse2::sse2_strstr_v2(line.data(), line.size(), pattern.data(),
+                                            pattern.size()) != std::string::npos;
+            }
+
+          private:
+            const std::string pattern;
+        };
+
+        // Check that two strings are equal.
+        class Equals {
+          public:
+            template <typename String>
+            explicit Equals(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) { return line == pattern; }
+
+          private:
+            const std::string pattern;
+        };
+    } // namespace sse2
+
+    namespace avx2 {
+        // Check that a searched string has a given pattern.
+        class StartsWith {
+          public:
+            template <typename String>
+            explicit StartsWith(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) {
+                if (line.size() < pattern.size()) { return false; }
+                return strncmp(line.data(), pattern.data(), pattern.size()) == 0;
+            }
+
+          private:
+            const std::string pattern;
+        };
+
+        // Search for a sub string.
+        class Contains {
+          public:
+            template <typename String>
+            explicit Contains(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) {
+                if (line.size() < pattern.size()) { return false; }
+                return line.find(pattern) != std::string::npos;
+            }
+
+          private:
+            const std::string pattern;
+        };
+
+        // Check that two strings are equal.
+        class Equals {
+          public:
+            template <typename String>
+            explicit Equals(String &&patt) : pattern(std::forward<String>(patt)) {}
+            bool operator()(const std::string &line) { return line == pattern; }
+
+          private:
+            const std::string pattern;
+        };
+    } // namespace avx2
 
 } // namespace utils
--- unittests/matchers.cpp
+++ unittests/matchers.cpp
@@ -8,38 +8,74 @@
 #define CATCH_CONFIG_MAIN
 #include "catch/catch.hpp"
 
-TEST_CASE("StartsWith", "StartsWith") {
+TEST_CASE("StartsWith", "baseline") {
     const std::string pattern("This");
     const std::string line1("This is the first line!");
     const std::string line2("Thisn't the first line!");
     const std::string line3("My first line!");
     const char *foo = NULL;
-    utils::StartsWith cons(pattern);
-    CHECK(cons(line1.c_str(), line1.size()));
-    CHECK(cons(line2.c_str(), line2.size()));
-    CHECK(!cons(line3.c_str(), line3.size()));
+    utils::baseline::StartsWith cons(pattern);
+    CHECK(cons(line1));
+    CHECK(cons(line2));
+    CHECK(!cons(line3));
 }
 
-TEST_CASE("Contains", "Contains") {
+TEST_CASE("Contains", "baseline") {
     const std::string pattern("the");
     const std::string line1("This is the first line!");
     const std::string line2("Thisn't the first line!");
     const std::string line3("My first line!");
     const char *foo = NULL;
-    utils::Contains cons(pattern);
-    CHECK(cons(line1.c_str(), line1.size()));
-    CHECK(cons(line2.c_str(), line2.size()));
-    CHECK(!cons(line3.c_str(), line3.size()));
+    utils::baseline::Contains cons(pattern);
+    CHECK(cons(line1));
+    CHECK(cons(line2));
+    CHECK(!cons(line3));
 }
 
-TEST_CASE("Equals", "Equals") {
+TEST_CASE("Equals", "baseline") {
     const std::string pattern("My first line!");
     const std::string line1("This is the first line!");
     const std::string line2("Thisn't the first line!");
     const std::string line3("My first line!");
     const char *foo = NULL;
-    utils::Equals cons(pattern);
-    CHECK(!cons(line1.c_str(), line1.size()));
-    CHECK(!cons(line2.c_str(), line2.size()));
-    CHECK(cons(line3.c_str(), line3.size()));
+    utils::baseline::Equals cons(pattern);
+    CHECK(!cons(line1));
+    CHECK(!cons(line2));
+    CHECK(cons(line3));
+}
+
+TEST_CASE("StartsWith-sse2", "") {
+    const std::string pattern("This");
+    const std::string line1("This is the first line!");
+    const std::string line2("Thisn't the first line!");
+    const std::string line3("My first line!");
+    const char *foo = NULL;
+    utils::sse2::StartsWith cons(pattern);
+    CHECK(cons(line1));
+    CHECK(cons(line2));
+    CHECK(!cons(line3));
+}
+
+TEST_CASE("Contains-sse2", "") {
+    const std::string pattern("the");
+    const std::string line1("This is the first line!");
+    const std::string line2("Thisn't the first line!");
+    const std::string line3("My first line!");
+    const char *foo = NULL;
+    utils::sse2::Contains cons(pattern);
+    CHECK(cons(line1));
+    CHECK(cons(line2));
+    CHECK(!cons(line3));
+}
+
+TEST_CASE("Equals-sse2", "") {
+    const std::string pattern("My first line!");
+    const std::string line1("This is the first line!");
+    const std::string line2("Thisn't the first line!");
+    const std::string line3("My first line!");
+    const char *foo = NULL;
+    utils::baseline::Equals cons(pattern);
+    CHECK(!cons(line1));
+    CHECK(!cons(line2));
+    CHECK(cons(line3));
 }
