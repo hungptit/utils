d912fd5aa336ec3ea67f70759d52c2475a88830b
Hung Dang <hungptit@gmail.com>
2018-03-26 13:18:29 -0400

    Add timestamp comparators

--- src/timeutils.hpp
+++ src/timeutils.hpp
@@ -6,44 +6,98 @@
 namespace utils {
     // A simple parser for timestamp.
     namespace timestamp {
-        namespace {
-            static constexpr char ZERO = '0';
-            static constexpr int SHIFT_2 = ZERO * 11;
-            static constexpr int SHIFT_4 = ZERO * 1111;
-            static constexpr int SHIFT_YEAR = 1900;
-            static constexpr int SHIFT_MONTH = 1;
+        static constexpr char ZERO = '0';
+        static constexpr int SHIFT_2 = ZERO * 11;
+        static constexpr int SHIFT_4 = ZERO * 1111;
+        static constexpr int SHIFT_YEAR = 1900;
+        static constexpr int SHIFT_MONTH = 1;
 
-            template <unsigned int N> int parse_digits(const char *ptr, const int initval) {
-                const int val = initval * 10 + ptr[0] - ZERO;
-                return parse_digits<N - 1>(++ptr, val);
-            }
-
-            template <> int parse_digits<1>(const char *ptr, const int initval) {
-                return initval * 10 + ptr[0] - ZERO;
-            }
-
-            int parse_two_digits(const char *begin) {
-                const char *ptr = begin;
-                return ptr[0] * 10 + ptr[1] - SHIFT_2;
-            }
-
-            int parse_four_digits(const char *begin) {
-                const char *ptr = begin;
-                return ptr[0] * 1000 + ptr[1] * 100 + ptr[2] * 10 + ptr[3] - SHIFT_4;
-            }
+        template <unsigned int N> int parse_digits(const char *ptr, const int initval) {
+            const int val = initval * 10 + ptr[0] - ZERO;
+            return parse_digits<N - 1>(++ptr, val);
         }
 
-        // Scribe log message has this format 02/04/2018 23:42:22.
-        struct scribe {
-            std::time_t operator()(const char *ptr) {
-                std::time_t t;
+        template <> int parse_digits<1>(const char *ptr, const int initval) {
+            return initval * 10 + ptr[0] - ZERO;
+        }
 
-                return t;
+        int parse_two_digits(const char *begin) {
+            const char *ptr = begin;
+            return ptr[0] * 10 + ptr[1] - SHIFT_2;
+        }
+
+        int parse_four_digits(const char *begin) {
+            const char *ptr = begin;
+            return ptr[0] * 1000 + ptr[1] * 100 + ptr[2] * 10 + ptr[3] - SHIFT_4;
+        }
+
+        // Scribe log message has this format "02/04/2018 23:42:22".
+        // This parse doesn't validate seperator characters.
+        struct scribe {
+            std::time_t operator()(const char *p) {
+                const char *ptr = p;
+
+                tm.tm_mon = parse_digits<2>(ptr, 0) - SHIFT_MONTH;
+                ptr += 3;
+
+                tm.tm_mday = parse_digits<2>(ptr, 0);
+                ptr += 3;
+
+                tm.tm_year = parse_digits<4>(ptr, 0) - SHIFT_YEAR;
+
+                ptr += 5;
+                tm.tm_hour = parse_digits<2>(ptr, 0);
+
+                ptr += 3;
+                tm.tm_min = parse_digits<2>(ptr, 0);
+
+                ptr += 3;
+                tm.tm_sec = parse_digits<2>(ptr, 0);
+
+                tm.tm_isdst = 0; // Do not care about day light saving.
+                return mktime(&tm);
             }
             struct tm tm;
         };
 
         struct rabbitmq {};
+
+        struct AllTimestamps {
+            bool operator()(const std::time_t t) { return true; }
+        };
+
+        struct OlderThan {
+            explicit OlderThan(const std::time_t t) : timestamp(t) {}
+            bool operator()(const std::time_t t) { return t > timestamp; }
+            const std::time_t timestamp;
+        };
+
+        struct NewerThan {
+			explicit NewerThan(const std::time_t t) : timestamp(t){}
+			bool operator()(const std::time_t t) {
+				return t < timestamp;
+			}
+			const std::time_t timestamp;
+		};
+
+        struct Equals {
+			explicit Equals(const std::time_t t) : timestamp(t){}
+			bool operator()(const std::time_t t) {
+				return t == timestamp;
+			}
+			const std::time_t timestamp;
+		};
+
+        struct Between {
+			Between(const std::time_t b, const std::time_t e) : begin(b), end(e){}
+
+			bool operator()(const std::time_t t) {
+				return (t > begin) && (t < end);
+			}
+			
+			const std::time_t begin;
+			const std::time_t end;
+		};
     }
 
     class TimeParser {
--- unittests/timeutils.cpp
+++ unittests/timeutils.cpp
@@ -8,6 +8,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch/catch.hpp"
 
+using Catch::Matchers::Equals;
+
 TEST_CASE("Parsing time strings", "Positive") {
     utils::TimeParser parser("%Y-%m-%d %H:%M:%S");
     std::string timestamp("2018-1-11 10:30:05");
@@ -26,4 +28,61 @@ TEST_CASE("Parsing digits", "Positive") {
 	REQUIRE(std::atoi(two.c_str()) == (utils::timestamp::parse_digits<2>(two.data(), 0)));
 	REQUIRE(std::atoi(three.c_str()) == (utils::timestamp::parse_digits<3>(three.data(), 0)));
 	REQUIRE(std::atoi(four.c_str()) == (utils::timestamp::parse_digits<4>(four.data(), 0)));
+}
+
+TEST_CASE("Parse timestamp", "scribe timestamp") {
+	std::string timestamp("02/04/2018 23:42:22");
+	utils::timestamp::scribe parser;
+	std::time_t t = parser(timestamp.data());
+
+	// Print out the parsed results
+	utils::TimePrinter printer("%Y-%m-%d %H:%M:%S");
+    printer(t);
+    fmt::print("{0} --> {1}\n", timestamp, printer.buffer);
+	CHECK_THAT(printer.buffer, Equals("2018-02-04 23:42:22"));
+}
+
+TEST_CASE("Comparators", "timestamp") {
+	std::string timestamp1("01/04/2018 13:42:22");
+	std::string timestamp2("02/04/2018 23:00:22");
+	std::string timestamp3("02/04/2018 15:31:00");
+	utils::timestamp::scribe parser;
+	std::time_t t1 = parser(timestamp1.data());
+	std::time_t t2 = parser(timestamp2.data());
+	std::time_t t3 = parser(timestamp3.data());
+
+	// Print out the parsed results
+	utils::TimePrinter printer("%Y-%m-%d %H:%M:%S");
+
+	printer(t1);
+    fmt::print("{0} --> {1}\n", timestamp1, printer.buffer);
+	CHECK_THAT(printer.buffer, Equals("2018-01-04 13:42:22"));
+
+	printer(t2);
+    fmt::print("{0} --> {1}\n", timestamp2, printer.buffer);
+	CHECK_THAT(printer.buffer, Equals("2018-02-04 23:00:22"));
+	
+	printer(t3);
+    fmt::print("{0} --> {1}\n", timestamp3, printer.buffer);
+	CHECK_THAT(printer.buffer, Equals("2018-02-04 15:31:00"));
+
+	// Check all time constraints.
+	utils::timestamp::AllTimestamps all;
+	utils::timestamp::OlderThan older_than(t3);
+	utils::timestamp::NewerThan newer_than(t3);
+	utils::timestamp::Between between(t1, t2);
+	utils::timestamp::Equals equals(t3);
+	CHECK(all(t1));
+
+	CHECK(!older_than(t1));
+	CHECK(older_than(t2));
+
+	CHECK(!newer_than(t2));
+	CHECK(newer_than(t1));
+
+	CHECK(!equals(t2));
+	CHECK(equals(t3));
+
+	CHECK(!between(t2));
+	CHECK(between(t3));
 }
