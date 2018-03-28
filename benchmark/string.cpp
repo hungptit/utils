0f57ee44ed3aeeda0f146d9778d645b12dcdf37a
Hung Dang <hungptit@gmail.com>
2018-03-27 23:00:28 -0400

    Separate avx2 code from normal code

--- benchmark/string.cpp
+++ benchmark/string.cpp
@@ -1,35 +1,31 @@
+#include "matchers.hpp"
 #include <benchmark/benchmark.h>
-#include "utils/matchers.hpp"
 
-const std::string data("[02/04/2018 23:42:22 job483.example.com db.db92.urgent 103212] {'LEVEL':'info','MESSAGE':'finished in 0.013\n','PREFIX':'WnfgrqwYG2AAAfC2Ag\\\/dXg'}");
+const std::string
+    data("[02/04/2018 23:42:22 job483.example.com db.db92.urgent 103212] "
+         "{'LEVEL':'info','MESSAGE':'finished in 0.013\n','PREFIX':'WnfgrqwYG2AAAfC2AgBgdXg'}");
 const std::string pattern("finished1234");
 
 // Basic implementation.
 void std_string_find(benchmark::State &state) {
-	utils::baseline::Contains contains(pattern);
-    for (auto _ : state) {
-		benchmark::DoNotOptimize(contains(data));
-	}
+    utils::baseline::Contains contains(pattern);
+    for (auto _ : state) { benchmark::DoNotOptimize(contains(data)); }
 }
 // Register the function as a benchmark
 BENCHMARK(std_string_find);
 
 void sse2_string_find(benchmark::State &state) {
-	utils::sse2::Contains contains(pattern);
-    for (auto _ : state) {
-		benchmark::DoNotOptimize(contains(data));
-	}
+    utils::sse2::Contains contains(pattern);
+    for (auto _ : state) { benchmark::DoNotOptimize(contains(data)); }
 }
 // Register the function as a benchmark
 BENCHMARK(sse2_string_find);
 
-void avx2_string_find(benchmark::State &state) {
-	utils::avx2::Contains contains(pattern);
-    for (auto _ : state) {
-		benchmark::DoNotOptimize(contains(data));
-	}
-}
-// Register the function as a benchmark
-BENCHMARK(avx2_string_find);
+// void avx2_string_find(benchmark::State &state) {
+//     utils::avx2::Contains contains(pattern);
+//     for (auto _ : state) { benchmark::DoNotOptimize(contains(data)); }
+// }
+// // Register the function as a benchmark
+// BENCHMARK(avx2_string_find);
 
 BENCHMARK_MAIN();
