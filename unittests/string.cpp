9ea60e40a75cf04757d2e8f30b6c39c189256f41
Hung Dang <hungptit@gmail.com>
2018-03-31 00:18:55 -0400

    Add unit tests for memchr.

--- unittests/string.cpp
+++ unittests/string.cpp
@@ -1,6 +1,7 @@
 #include "fmt/format.h"
 #include <string>
 
+#include "fmt/format.h"
 #include "memchr.hpp"
 
 #define CATCH_CONFIG_MAIN
@@ -12,4 +13,20 @@ TEST_CASE("avx2_memchr", "memchr") {
     const std::string line(
         "[02/04/2018 23:42:22 job483.athenahealth.com db.db92.urgent 103213] "
         "{'LEVEL':'info','MESSAGE':'received\n','PREFIX':'WnfgrqwYFLwAAPQIAAAAWQ'}");
+
+	// Simple tests for memchr
+	CHECK(memchr(line.data(), '*', line.size()) == NULL);
+	CHECK(memchr(line.data(), '2', line.size()) != NULL);
+	CHECK(static_cast<const char*>(memchr(line.data(), '2', line.size())) == (line.data() + 2));
+	CHECK(static_cast<const char*>(memchr(line.data(), ' ', line.size())) == (line.data() + 11));
+
+	// Simple tests for memchr_avx2
+	CHECK(memchr_avx2(line.data(), '*', line.size()) == NULL);
+	CHECK(memchr_avx2(line.data(), '2', line.size()) != NULL);
+	CHECK(static_cast<const char*>(memchr_avx2(line.data(), '2', line.size())) == (line.data() + 2));
+	CHECK(static_cast<const char*>(memchr_avx2(line.data(), ' ', line.size())) == (line.data() + 11));
+
+	// Simple manual test
+	auto ptr = memchr_avx2(line.data(), ']', line.size());
+	fmt::print("Data: {}\n", ptr + 1);
 }
