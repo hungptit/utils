a82bc11d45a714b77631fe83171ba669a5c98b5c
Hung Dang <hungptit@gmail.com>
2018-04-08 00:47:42 -0400

    Add a missing header.

---
+++ src/strcmp.hpp
@@ -0,0 +1,51 @@
+// SIMD implementations of == operator
+// TODO: Implement > and < operator for std::string.
+#pragma once
+#include <cstring>
+#include <immintrin.h>
+
+namespace utils {
+    // An SSE2 implementation of strncmp. 
+    bool strncmp_sse2(const char *first, const char *second, const size_t len) {
+        size_t idx = 0;
+		
+        // Use SSE2 for the first len - (len%16) characters.
+        constexpr size_t NBYTES = 16;
+        const size_t res = len % NBYTES;
+        const size_t N = len - res;
+        if (N > 0) {
+            for (; idx < N; idx += NBYTES) {
+                __m128i x = _mm_loadu_si128(reinterpret_cast<const __m128i *>(first + idx));
+                __m128i y = _mm_loadu_si128(reinterpret_cast<const __m128i *>(second + idx));
+                __m128i results = _mm_cmpeq_epi8(x, y);
+                int mask = _mm_movemask_epi8(results);
+                if (mask) { return false; }
+            }
+        }
+
+        // Fallback to memcmp for the rest.
+        return memcmp(first + idx, second + idx, res) == 0;
+    }
+	
+    // An AVX2 implementation of strncmp. 
+    bool strncmp_avx2(const char *first, const char *second, const size_t len) {
+        size_t idx = 0;
+		
+        // Use AVX2 for the first len - (len %32) characters.
+        constexpr size_t NBYTES = 32;
+        const size_t res = len % NBYTES;
+        const size_t N = len - res;
+        if (N > 0) {
+            for (; idx < N; idx += NBYTES) {
+                __m256i x = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(first + idx));
+                __m256i y = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(second + idx));
+                __m256i results = _mm256_cmpeq_epi8(x, y);
+                int mask = _mm256_movemask_epi8(results);
+                if (mask) { return false; }
+            }
+        }
+
+        // Fallback to memcmp for the rest.
+        return memcmp(first + idx, second + idx, res) == 0;
+    }
+}
