c3b9c0313947f22e9d8882aa72940000aea355ef
Hung Dang <hungptit@gmail.com>
2018-05-16 18:21:16 -0400

    Add more benchmark for lookup table. Below are the benchmark results
    2018-05-16 18:21:52
    Running ./lookuptable
    Run on (8 X 2200 MHz CPU s)
    CPU Caches:
      L1 Data 32K (x4)
      L1 Instruction 32K (x4)
      L2 Unified 262K (x4)
      L3 Unified 6291K (x1)
    -------------------------------------------------------------------------
    Benchmark                                  Time           CPU Iterations
    -------------------------------------------------------------------------
    benchmark_baseline                        77 ns         77 ns    8991651
    benchmark_find_std                        87 ns         87 ns    6674549
    benchmark_find_recursive_template         75 ns         75 ns    9395595
    benchmark_find_strcmp                     73 ns         73 ns    9645593
    benchmark_find_set                       158 ns        158 ns    4516420
    benchmark_find_hash                      156 ns        156 ns    4519395

--- benchmark/lookuptable.cpp
+++ benchmark/lookuptable.cpp
@@ -1,39 +1,83 @@
 #include <array>
 #include <benchmark/benchmark.h>
+#include <set>
 #include <string>
-#include "strcmp.hpp"
+#include <unordered_set>
+
+#include "string.hpp"
+#include "string_avx2.hpp"
 
 const std::array<std::string, 16> lookup_table = {
     {"PREFIX", "MESSAGE", "LEVEL", "RAW_ERROR", "RESOURCENAME", "SUBJECT", "REQUEST",
      "SENDTIME", "DESTINATION", "SERIALIZETIME", "SENDERID", "EXCHANGE"}};
-const std::string lookup_string = "PREFIX,MESSAGE,LEVEL,RAW_ERROR,RESOURCENAME,SUBJECT,REQUEST,"
-                                  "SENDTIME,DESTINATION,SERIALIZETIME,SENDERID,EXCHANGE";
+const std::set<std::string> set_lookup_table(lookup_table.cbegin(), lookup_table.cend());
+const std::set<std::string> hash_lookup_table(lookup_table.cbegin(), lookup_table.cend());
 
 bool is_equal(const std::string &first, const std::string &second) {
-    if (first.size() != second.size()) return false;
-    return utils::strncmp_avx2(first.data(), second.data(), first.size());
+    return (first.size() != second.size()) &&
+           strncmp(first.data(), second.data(), first.size());
 }
 
-bool find_slow(const std::string &pattern) {
-    return std::find(lookup_table.cbegin(), lookup_table.cend(), pattern) !=
-           lookup_table.cend();
-}
+namespace {
+    struct Baseline {
+        bool operator()(const std::string &pattern) {
+			for (auto const & item : lookup_table) {
+				if (item == pattern) return true;
+			}
+            return false;
+        }
+        const std::array<std::string, 16> lookup_table = {
+            {"PREFIX", "MESSAGE", "LEVEL", "RAW_ERROR", "RESOURCENAME", "SUBJECT", "REQUEST",
+             "SENDTIME", "DESTINATION", "SERIALIZETIME", "SENDERID", "EXCHANGE", "FOO1", "FOO2",
+             "FOO3", "FOO4"}};
+    };
 
-template <int N> bool find_fast(const std::string &pattern) {
-    return (lookup_table[N] == pattern) || find_fast<N - 1>(pattern);
-}
+	struct StdFind {
+        bool operator()(const std::string &pattern) {
+            return std::find(lookup_table.cbegin(), lookup_table.cend(), pattern) != lookup_table.cend();
+        }
+        const std::array<std::string, 16> lookup_table = {
+            {"PREFIX", "MESSAGE", "LEVEL", "RAW_ERROR", "RESOURCENAME", "SUBJECT", "REQUEST",
+             "SENDTIME", "DESTINATION", "SERIALIZETIME", "SENDERID", "EXCHANGE", "FOO1", "FOO2",
+             "FOO3", "FOO4"}};
+    };
 
-template <> bool find_fast<0>(const std::string &pattern) {
-    return (lookup_table[0] == pattern);
-}
+    template <int N> bool find_fast(const std::string &pattern) {
+        return (lookup_table[N] == pattern) || find_fast<N - 1>(pattern);
+    }
 
-template <int N> bool find_fast_strcmp(const std::string &pattern) {
-    return (lookup_table[N] == pattern) || find_fast_strcmp<N - 1>(pattern);
-}
+    template <> bool find_fast<0>(const std::string &pattern) {
+        return (lookup_table[0] == pattern);
+    }
 
-template <> bool find_fast_strcmp<0>(const std::string &pattern) {
-    return is_equal(lookup_table[0], pattern);
-}
+    struct RecursiveTemplate {
+        bool operator()(const std::string &pattern) { return find_fast<15>(pattern); }
+        const std::array<std::string, 16> lookup_table = {
+            {"PREFIX", "MESSAGE", "LEVEL", "RAW_ERROR", "RESOURCENAME", "SUBJECT", "REQUEST",
+             "SENDTIME", "DESTINATION", "SERIALIZETIME", "SENDERID", "EXCHANGE", "FOO1", "FOO2",
+             "FOO3", "FOO4"}};
+    };
+
+    template <int N> bool find_fast_strcmp(const std::string &pattern) {
+        return (lookup_table[N] == pattern) || find_fast_strcmp<N - 1>(pattern);
+    }
+
+    template <> bool find_fast_strcmp<0>(const std::string &pattern) {
+        return is_equal(lookup_table[0], pattern);
+    }
+
+	template <int N>
+    struct RecursiveTemplateStrcmp {
+        bool operator()(const std::string &pattern) {
+            return find_fast_strcmp<N - 1>(pattern);
+        }
+        const std::array<std::string, N> lookup_table = {
+            {"PREFIX", "MESSAGE", "LEVEL", "RAW_ERROR", "RESOURCENAME", "SUBJECT", "REQUEST",
+             "SENDTIME", "DESTINATION", "SERIALIZETIME", "SENDERID", "EXCHANGE", "FOO1", "FOO2",
+             "FOO3", "FOO4"}};
+    };
+
+} // namespace
 
 const std::string pattern1("RESOURCE");
 const std::string pattern2("SENDERID");
@@ -41,40 +85,74 @@ const std::string pattern3("PREFIX");
 const std::string pattern4("FOOOOO");
 const std::string pattern5("EXCHANGE");
 
-void benchmark_find_slow(benchmark::State &state) {
+void benchmark_baseline(benchmark::State &state) {
+    Baseline isok;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(find_slow(pattern1));
-        benchmark::DoNotOptimize(find_slow(pattern2));
-        benchmark::DoNotOptimize(find_slow(pattern3));
-        benchmark::DoNotOptimize(find_slow(pattern4));
-        benchmark::DoNotOptimize(find_slow(pattern5));
+        benchmark::DoNotOptimize(isok(pattern1));
+        benchmark::DoNotOptimize(isok(pattern2));
+        benchmark::DoNotOptimize(isok(pattern3));
+        benchmark::DoNotOptimize(isok(pattern4));
+        benchmark::DoNotOptimize(isok(pattern5));
     }
 }
-BENCHMARK(benchmark_find_slow);
+BENCHMARK(benchmark_baseline);
 
-void benchmark_find_fast(benchmark::State &state) {
+void benchmark_find_std(benchmark::State &state) {
+    StdFind isok;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(find_fast<7>(pattern1));
-        benchmark::DoNotOptimize(find_fast<7>(pattern2));
-        benchmark::DoNotOptimize(find_fast<7>(pattern3));
-        benchmark::DoNotOptimize(find_fast<7>(pattern4));
-        benchmark::DoNotOptimize(find_fast<7>(pattern5));
+        benchmark::DoNotOptimize(isok(pattern1));
+        benchmark::DoNotOptimize(isok(pattern2));
+        benchmark::DoNotOptimize(isok(pattern3));
+        benchmark::DoNotOptimize(isok(pattern4));
+        benchmark::DoNotOptimize(isok(pattern5));
     }
 }
-
-BENCHMARK(benchmark_find_fast);
+BENCHMARK(benchmark_find_std);
+ 
+void benchmark_find_recursive_template(benchmark::State &state) {
+    RecursiveTemplate isok;
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(isok(pattern1));
+        benchmark::DoNotOptimize(isok(pattern2));
+        benchmark::DoNotOptimize(isok(pattern3));
+        benchmark::DoNotOptimize(isok(pattern4));
+        benchmark::DoNotOptimize(isok(pattern5));
+    }
+}
+BENCHMARK(benchmark_find_recursive_template);
 
 void benchmark_find_strcmp(benchmark::State &state) {
+    RecursiveTemplateStrcmp<16> isok;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(find_fast_strcmp<7>(pattern1));
-        benchmark::DoNotOptimize(find_fast_strcmp<7>(pattern2));
-        benchmark::DoNotOptimize(find_fast_strcmp<7>(pattern3));
-        benchmark::DoNotOptimize(find_fast_strcmp<7>(pattern4));
-        benchmark::DoNotOptimize(find_fast_strcmp<7>(pattern5));
+        benchmark::DoNotOptimize(isok(pattern1));
+        benchmark::DoNotOptimize(isok(pattern2));
+        benchmark::DoNotOptimize(isok(pattern3));
+        benchmark::DoNotOptimize(isok(pattern4));
+        benchmark::DoNotOptimize(isok(pattern5));
     }
 }
-
 BENCHMARK(benchmark_find_strcmp);
 
+void benchmark_find_set(benchmark::State &state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(set_lookup_table.find(pattern1) != set_lookup_table.end());
+        benchmark::DoNotOptimize(set_lookup_table.find(pattern2) != set_lookup_table.end());
+        benchmark::DoNotOptimize(set_lookup_table.find(pattern3) != set_lookup_table.end());
+        benchmark::DoNotOptimize(set_lookup_table.find(pattern4) != set_lookup_table.end());
+        benchmark::DoNotOptimize(set_lookup_table.find(pattern5) != set_lookup_table.end());
+    }
+}
+BENCHMARK(benchmark_find_set);
+
+void benchmark_find_hash(benchmark::State &state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(hash_lookup_table.find(pattern1) != hash_lookup_table.end());
+        benchmark::DoNotOptimize(hash_lookup_table.find(pattern2) != hash_lookup_table.end());
+        benchmark::DoNotOptimize(hash_lookup_table.find(pattern3) != hash_lookup_table.end());
+        benchmark::DoNotOptimize(hash_lookup_table.find(pattern4) != hash_lookup_table.end());
+        benchmark::DoNotOptimize(hash_lookup_table.find(pattern5) != hash_lookup_table.end());
+    }
+}
+BENCHMARK(benchmark_find_hash);
 
 BENCHMARK_MAIN();
